e8cd775eef14bbc300fdabf493cfe712a4d37d99
Girish <girishsawant999.gs@gmail.com>
2020-09-24 00:39:12 +0530

    Final touch

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -4,13 +4,21 @@ import './addTask.css';
 function addTask({ addTask }) {
   const formSubmit = (e) => {
     e.preventDefault();
-    addTask(e.target.task.value);
-    e.target.task.value = '';
+    if (e.target.task.value.match(/^(?!\s*$).+/g)) {
+      addTask(e.target.task.value);
+      e.target.task.value = '';
+    }
   };
   return (
     <div className="addTask">
       <form action="" onSubmit={formSubmit}>
-        <input type="text" name="task" id="input_addTask" />
+        <input
+          type="text"
+          name="task"
+          id="input_addTask"
+          placeholder="Enter the task"
+          autoComplete="off"
+        />
         <button className="disable-select" type="submit">
           Add Task <span class="material-icons">add</span>
         </button>
