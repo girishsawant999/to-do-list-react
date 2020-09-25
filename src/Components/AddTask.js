ee00530fa1de7340f8648fb03c3f6dcd5d970c0d
Girish <girishsawant999.gs@gmail.com>
2020-09-25 23:26:42 +0530

    Second version

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -4,14 +4,20 @@ import './addTask.css';
 function addTask({ addTask }) {
   const formSubmit = (e) => {
     e.preventDefault();
-    addTask(e.target.task.value);
-    e.target.task.value = '';
+    const re = /^(?!\s*$).+/g;
+    if (re.test(e.target.task.value)) {
+      addTask(e.target.task.value);
+      e.target.task.value = '';
+    }
   };
   return (
     <div className="addTask">
       <form action="" onSubmit={formSubmit}>
         <input type="text" name="task" id="input_addTask" />
-        <button className="disable-select" type="submit">
+        <button
+          className="disable-select"
+          type="submit"
+          onKeyDown={(event) => event.preventDefault()}>
           Add Task <span class="material-icons">add</span>
         </button>
       </form>
