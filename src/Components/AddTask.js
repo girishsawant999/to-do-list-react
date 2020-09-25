8d16fab831dedeea1a12c4e38d1bcdfdd78b058f
Girish <girishsawant999.gs@gmail.com>
2020-09-25 23:02:55 +0530

    Revert "Final touch"
    This reverts commit e8cd775eef14bbc300fdabf493cfe712a4d37d99.

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -4,21 +4,13 @@ import './addTask.css';
 function addTask({ addTask }) {
   const formSubmit = (e) => {
     e.preventDefault();
-    if (e.target.task.value.match(/^(?!\s*$).+/g)) {
-      addTask(e.target.task.value);
-      e.target.task.value = '';
-    }
+    addTask(e.target.task.value);
+    e.target.task.value = '';
   };
   return (
     <div className="addTask">
       <form action="" onSubmit={formSubmit}>
-        <input
-          type="text"
-          name="task"
-          id="input_addTask"
-          placeholder="Enter the task"
-          autoComplete="off"
-        />
+        <input type="text" name="task" id="input_addTask" />
         <button className="disable-select" type="submit">
           Add Task <span class="material-icons">add</span>
         </button>
